f2b839a13dba51f8f966b4978a09d48cfedd7383
Lee SH <snghyun331@gmail.com>
2023-04-26 23:45:35 +0900

    0426

---
+++ back/src/routers/educationRouter.js
@@ -0,0 +1,114 @@
+// import is from "@sindresorhus/is";
+// import { Router } from "express";
+// import { login_required } from "../middlewares/login_required";
+// import { educationService } from "../services/educationService";
+
+// const educationRouter = Router();
+
+// // 학력 정보 추가
+// educationRouter.post("/educations", login_required, async function (req, res, next) {
+//   // @sindresorhus/is 패키지의 is.emptyObject 함수를 사용해 HTTP 요청의 body가 비어있는지 검사함(비었으면 에러 발생)
+//   try {
+//     if (is.emptyObject(req.body)) {
+//       throw new Error(
+//         "headers의 Content-Type을 application/json으로 설정해주세요"
+//       );
+//     }
+
+//     // req (request) 에서 데이터 가져오기
+//     const school = req.body.school;
+//     const major = req.body.major;
+//     const degree = req.body.degree;
+
+//     // 유저의 id 가져오기
+//     // login_required 미들웨어에서 설정한 currentUserId 속성을 사용해 현재 로그인한 사용자의 ID 값을 가져옴
+//     const user_id = req.currentUserId;
+
+//     // 위 데이터를 유저 db에 추가하기
+//     // educationService의 addEducation 함수를 호출해 해당 요청 사용자의 학력 정보를 추가함
+//     const newEducation = await educationService.addEducation({
+//       user_id,
+//       school,
+//       major,
+//       degree,
+//     });
+
+//     // educationService.addEducation 메소드 호출하고 반환된 결과의 에러가 존재하면 에러 객체를 던짐
+//     if (newEducation.errorMessage) {
+//       throw new Error(newEducation.errorMessage);
+//     }
+
+//     // 성공적인 HTTP 응답을 생성. status(201)로 201 Created 상태코드를, json()으로 newEducation 객체를 JSON 형태로 반환
+//     // 에러 발생시 next함수로 다음 미들웨어에 에러 객체를 던짐
+//     res.status(201).json(newEducation);
+//   } catch (error) {
+//     next(error);
+//   }
+// });
+
+
+// // 학력 정보 조회
+// educationRouter.get("/educations/:id", login_required, async function (req, res, next) {
+//   try {
+//     // URL 매개변수로 전달된 id값 할당
+//     const education_id = req.params.id;
+//     // educationService.getEducation 메서드로 education_id가 가진 학력정보를 가져옴
+//     const education = await educationService.getEducation({ education_id });
+//     // 에러메시지 존재시 에러 던짐
+//     if (education.errorMessage) {
+//       throw new Error(education.errorMessage);
+//     }
+    
+//     // 성공적으로 학력 정보 가져오면 HTTP응답코드(200)와 함께 응답, 에러 발생시 next로 미들웨어 함수로 에러 던짐
+//     res.status(200).send(education);
+//   } catch (error) {
+//     next(error);
+//   }
+// });
+
+// // 학력 정보 수정
+// educationRouter.put("/educations/:id", login_required, async function (req, res, next) {
+//   try {
+//     const education_id = req.params.id;
+//     // HTTP 요청의 body에서 각 속성을 가져오고, 존재하지 않으면 null 반환
+//     const school = req.body.school ?? null;
+//     const major = req.body.major ?? null;
+//     const degree = req.body.degree ?? null;
+    
+//     //업데이트 할 데이터를 객체 형태로 만들어 변수에 할당
+//     const toUpdate = { school, major, degree };
+
+//     //educationService.setEducation 함수 호출하여 education_id와 toUpdate 객체를 전달하고 변수에 할당
+//     // 해당 사용자 아이디로 사용자 정보를 db에서 찾아 업데이트함. 업데이트 요소가 없을 시 생략함
+//     const updatedEducation = await educationService.setEducation({ education_id, toUpdate });
+
+//     // 에러메시지 존재시 에러 던짐
+//     if (updatedEducation.errorMessage) {
+//       throw new Error(updatedEducation.errorMessage);
+//     }
+
+//     // 성공하면 HTTP응답코드(200)와 함께 응답, 에러 발생시 next로 미들웨어 함수로 에러 던짐
+//     res.status(200).json(updatedEducation);
+//   } catch (error) {
+//     next(error);
+//   }
+// });
+
+// // 선택한 특정 유저의 학력 정보 조회
+// educationRouter.get("/educations/user/:id", login_required, async function (req, res, next) {
+//   try {
+//     const user_id = req.params.id;
+//     // req.prams.id를 통해 요청한 사용자ID를 가져오고, getEducation 함수를 호출하여 해당 사용자 학력 정보 가져옴
+//     const educations = await educationService.getEducations({ user_id });
+    
+//     if (educations.errorMessage) {
+//       throw new Error(educations.errorMessage);
+//     }
+
+//     res.status(200).send(educations);
+//   } catch (error) {
+//     next(error);
+//   }
+// });
+
+// export { educationRouter };
