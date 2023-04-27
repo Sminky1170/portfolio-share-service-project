70ced26a301fe1edb0eef5f7d2107ac5a8520405
Lee SH <snghyun331@gmail.com>
2023-04-27 16:10:01 +0900

    최종 수정

--- back/src/db/schemas/education.js
+++ back/src/db/schemas/education.js
@@ -2,22 +2,25 @@ import { Schema, model } from "mongoose";
 
 const EducationSchema = new Schema(
   {
-    schoolName: {  
+    id: {
       type: String,
       required: true,
     },
-    major: {      
+    userId:{
+      type: String,
+      required: true,
+    },
+    schoolName: {
+      type: String,
+      required: true,
+    },
+    major: {
       type: String,
       required: true,
     },
     degree: {
       type: String,
       required: true,
-    },
-    description: {
-      type: String,
-      required: false,
-      default: "설명이 아직 없습니다. 추가해 주세요.",
     },
   },
   {
--- back/src/services/educationService.js
+++ back/src/services/educationService.js
@@ -2,7 +2,7 @@ import { User, Education } from "../db";
 import { v4 as uuidv4 } from "uuid";
 
 class educationService {
-  static async addEducation({ user_id, schoolName, major, degree}) {
+  static async addEducation({ user_id, school, major, degree}) {
     // DB에서 해당 User를 찾는다.
     const user = await User.findById({ user_id }); 
     // 만약 DB에 해당 user_id가 존재하지 않을 경우,
@@ -13,7 +13,7 @@ class educationService {
     // DB에 해당 user_id가 존재할 경우,
 
     // 먼저, DB에서 해당 유저의 학적 정보를 확인해서, 만약 추가하려는 학적 정보가 이미 DB에 존재할 경우
-    const obj = { user_id, schoolName, major, degree }
+    const obj = { user_id, school, major, degree }
     const find_education = await Education.findByObj(obj)
     if(find_education) {
         const errorMessage = "이미 존재하는 학적입니다. 다른 학적을 추가해주세요."
@@ -22,10 +22,10 @@ class educationService {
 
     // DB에 추가하려는 학적 정보가 없다면, 새로 추가 가능
     const id = uuidv4();
-    const newEducation = { id, user_id, schoolName, major, degree};
+    const newEducation = { id, user_id, school, major, degree};
     const createdEducation = await Education.createEducation({ newEducation });
     createdEducation.errorMessage = null;
-    
+
     return createdEducation;
   }
 
@@ -44,10 +44,10 @@ class educationService {
       const errorMessage = "해당 학력 정보를 찾을 수 없습니다. 다시 한 번 확인해 주세요.";
       return { errorMessage };
     }
-    // 업데이트 대상에 schoolName이 있다면, 즉 schoolName 값이 null 이 아니라면 업데이트 진행
-    if (toUpdate.schoolName) {
-      const fieldToUpdate = "schoolName";
-      const newValue = toUpdate.schoolName;
+    // 업데이트 대상에 school이 있다면, 즉 school 값이 null 이 아니라면 업데이트 진행
+    if (toUpdate.school) {
+      const fieldToUpdate = "school";
+      const newValue = toUpdate.school;
       education = await Education.update({ education_id, fieldToUpdate, newValue });
     }
 
