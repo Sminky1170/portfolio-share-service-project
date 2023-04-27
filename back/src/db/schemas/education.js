99616ae9af12c69d5a9a8a12c0f5f94892722df9
Lee SH <snghyun331@gmail.com>
2023-04-27 15:59:19 +0900

    service 수정, model도 조금 수정 및 추가

--- back/src/db/schemas/education.js
+++ back/src/db/schemas/education.js
@@ -1,30 +1,30 @@
-// import { Schema, model } from "mongoose";
+import { Schema, model } from "mongoose";
 
-// const EducationSchema = new Schema(
-//   {
-//     schoolName: {
-//       type: String,
-//       required: true,
-//     },
-//     major: {
-//       type: String,
-//       required: true,
-//     },
-//     degree: {
-//       type: String,
-//       required: true,
-//     },
-//     description: {
-//       type: String,
-//       required: false,
-//       default: "설명이 아직 없습니다. 추가해 주세요.",
-//     },
-//   },
-//   {
-//     timestamps: true,
-//   }
-// );
+const EducationSchema = new Schema(
+  {
+    schoolName: {  
+      type: String,
+      required: true,
+    },
+    major: {      
+      type: String,
+      required: true,
+    },
+    degree: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: false,
+      default: "설명이 아직 없습니다. 추가해 주세요.",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-// const EducationModel = model("Education", EducationSchema);
+const EducationModel = model("Education", EducationSchema);
 
-// export { EducationModel };
+export { EducationModel };
