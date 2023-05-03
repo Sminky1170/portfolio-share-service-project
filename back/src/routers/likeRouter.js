bb416b2b90166d71f2dd6ff8670c2d20c5e96e55
[AI7/2반]김희찬 <khc0023@gmail.com>
2023-05-03 10:18:54 +0900

    like 구현중

--- back/src/routers/likeRouter.js
+++ back/src/routers/likeRouter.js
@@ -5,55 +5,66 @@ import { LikeService } from "../services/LikeService.js";
 import { userAuthService } from "../services/userService.js"
 const likeRouter = Router();
 
-// 좋아요 생성 API
-likeRouter.post('/likes', async (req, res) => {
+// 좋아요 생성
+likeRouter.post('/likes', login_required, async (req, res, next) => {
+  try {
+    if (is.emptyObject(req.body)) {
+      throw new Error(
+        "headers의 Content-Type을 application/json으로 설정해주세요"
+      );
+    }
+    
+    const { following_user_id, followed_user_id } = req.body;
+
+    const newLike = await LikeService.createLike({
+      following_user_id,
+      followed_user_id
+    });
+
+    return res.status(201).json(newLike);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// 좋아요 삭제
+likeRouter.delete('/likes', login_required, async (req, res, next) => {
   try {
     const { following_user_id, followed_user_id } = req.body;
-    const createdLike = await LikeService.createLike({ following_user_id, followed_user_id });
-    res.status(201).json(createdLike);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: '서버 에러' });
-  }
-});
-
-// 좋아요 삭제 API
-likeRouter.delete('/likes', async (req, res) => {
-  try {
-    const { following_user_id, followed_user_id } = req.params;
     const isDeleted = await LikeService.deleteLike({ following_user_id, followed_user_id });
     if (isDeleted) {
-      res.status(204).end();
+      return res.status(204).end();
     } else {
-      res.status(404).json({ message: '삭제할 좋아요가 없습니다.' });
+      return res.status(404).json({ message: '삭제할 좋아요가 없습니다.' });
     }
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: '서버 에러' });
+  } catch (error) {
+    next(error);
   }
 });
 
-// 로그인한 유저가 좋아요한 특정 유저 조회 API
-likeRouter.get('/likes/:followed_user_id', async (req, res) => {
+// 로그인한 유저가 좋아요한 특정 유저 조회
+likeRouter.get('/likes/:following_user_id', login_required, async (req, res, next) => {
   try {
     const { following_user_id } = req.params;
     const likes = await LikeService.getLikesByFollowingUserId({ following_user_id });
-    res.status(200).json(likes);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: '서버 에러' });
+    
+    return res.status(200).json(likes);
+  } catch (error) {
+  next(error);
   }
 });
 
-// 특정 유저를 좋아요한 유저 조회 API
-likeRouter.get('/likelist/:followed_user_id', async (req, res) => {
+// 특정 유저를 좋아요한 유저 조회
+likeRouter.get('/likelist/:followed_user_id', login_required, async (req, res, next) => {
   try {
     const { followed_user_id } = req.params;
     const likes = await LikeService.getLikesByFollowedUserId({ followed_user_id });
-    res.status(200).json(likes);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: '서버 에러' });
+    if (likes.errorMessage) {
+      throw new Error(likes.errorMessage);
+    }
+    return res.status(200).json(likes);
+  } catch (error) {
+    next(error);
   }
 });
 
